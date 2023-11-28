163ffc8b1f04ec26be537b2b9df8c8623b734cea
jmneko <jmunhiztarrio@gmail.com>
2023-11-28 12:38:30 +0100

    Diseño de alumno-card

--- src/app/components/alumno-card/alumno-card.component.ts
+++ src/app/components/alumno-card/alumno-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from 'src/app/models/interfaces/user.interface';
 
 @Component({
   selector: 'app-alumno-card',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./alumno-card.component.css']
 })
 export class AlumnoCardComponent {
+
+  @Input() miUsuario!: IUser;
 
 }
