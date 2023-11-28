cd002313739ef5d7a1f9676b9c275011924fdc2a
jmneko <jmunhiztarrio@gmail.com>
2023-11-28 12:51:50 +0100

    Maquetacion y funciomalidad de lista-alumnos

--- src/app/pages/lista-alumnos/lista-alumnos.component.ts
+++ src/app/pages/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUser } from 'src/app/models/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-alumnos.component.css']
 })
 export class ListaAlumnosComponent {
+
+  arrUsuario: IUser[] = [];
+  usuariosService = inject(UsersService);
+
+  async ngOnInit(): Promise<void> {
+    
+    
+    try {
+      const profesorId = 2;
+
+      const alumnos = await this.usuariosService.getAlumnosByProfesorId(profesorId);
+
+      this.arrUsuario = alumnos;
+      console.log(this.arrUsuario)
+    } catch (error) {
+      console.log(error);
+    }
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*try {
+      const allUsuarios = await this.usuariosService.getAll();
+      this.arrUsuario = allUsuarios.filter(usuario => usuario.rol === 'alumn');
+      console.log(this.arrUsuario)
+    } catch (error) {
+    }
+  }
+
+  clasificarUsuarios(usuarios: Usuario[]): Usuario[] {
+    const aceptados = usuarios.filter(usuario => usuario.activo === true);
+    console.log()
+    const solicitud = usuarios.filter(usuario => usuario.activo === false);
+
+    return [...aceptados, ...solicitud];*/
+  }
 
 }
