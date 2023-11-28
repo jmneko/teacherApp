2c9839f204c77b65f9a450269aef2810dc903d5f
jmneko <jmunhiztarrio@gmail.com>
2023-11-28 12:23:42 +0100

    configuracion app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { ClasesAlumnoComponent } from './pages/clases-alumno/clases-alumno.component';
+import { ClasesCardComponent } from './components/clases-card/clases-card.component';
+import { AlumnoCardComponent } from './components/alumno-card/alumno-card.component';
+import { FormUsuariosComponent } from './components/form-usuarios/form-usuarios.component';
+import { ListaAlumnosComponent } from './pages/lista-alumnos/lista-alumnos.component';
+import { PeticionCardComponent } from './components/peticion-card/peticion-card.component';
+import { P404Component } from './pages/p404/p404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'clases'},
+  { path: 'usuarios/:usuarioId', component: UsuarioComponent },
+  { path:'clases', component: ClasesAlumnoComponent },
+  { path: 'clasesCard', component: ClasesCardComponent },
+  { path: 'alumnoCard', component: AlumnoCardComponent},
+  { path: 'updateUsuario/:usuarioId', component: FormUsuariosComponent },
+  { path: ':profesorId/alumnos', component: ListaAlumnosComponent },
+  { path: 'peticionCard', component: PeticionCardComponent },
+  { path: '**', component: P404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
