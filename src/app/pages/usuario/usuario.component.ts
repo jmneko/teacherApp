1d2368de6a22979809a21033f5bc8db0c00cf295
jmneko <jmunhiztarrio@gmail.com>
2023-11-28 12:55:44 +0100

    Funcionalidad y maquetacion usuario

--- src/app/pages/usuario/usuario.component.ts
+++ src/app/pages/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from 'src/app/models/interfaces/user.interface';
 
 @Component({
   selector: 'app-usuario',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent {
+
+  @Input() miUsuario: IUser | any;
 
 }
